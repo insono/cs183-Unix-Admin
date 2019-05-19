abb2bf3cab4ad06017ceb98ee3cfde4fe8689673
Andrew <ayuan003@ucr.edu>
2019-05-18 20:32:39 -0700

    finished hw3

--- hw3/three.pl
+++ hw3/three.pl
@@ -6,9 +6,26 @@ my $filename = $ARGV[0];
 open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
 
+my %from;
+my %to;
+
 while (my $row = <$fh>) {
   chomp $row;
-  if ($row =~ /from=</) {
-    my $from 
+  if ($row =~ /from=</) { 
+    my ($fromExtract) = ($row =~ /(from=<[\w\.\-\|]+@[\w\.\-\|]+>)/);
+    if (defined $fromExtract) {
+      $from{$fromExtract}++;
+    }
+  }
+  if ($row =~ /to=</) {
+    my ($toExtract) = ($row =~ /(to=<[\w\.\-\|]+@[\w\.\-\|]+>)/);
+    if (defined $toExtract) {
+      $to{$toExtract}++;
+    }
+  }
 }
-#print Dumper(\%known);
+
+my @keys = sort { $from{$b} <=> $from{$a} } keys(%from);
+foreach my $key (@keys[0..4]) { print "$key $from{$key}\n"; }
+@keys = sort { $from{$b} <=> $from{$a} } keys(%to);
+foreach my $key (@keys[0..4]) { print "$key $to{$key}\n"; }
